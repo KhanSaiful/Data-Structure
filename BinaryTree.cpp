72a83c7d4f774c7bb4f58b289867cee7bac6cf11
Saiful Islam Khan <anikme71@gmail.com>
2019-01-23 14:58:14 +0600

    Adding Binary tree

---
+++ BinaryTree.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *newNode(int data)
+{
+    struct node *node = (struct node*)malloc(sizeof(struct node));
+
+    node->data = data;
+
+    node->left = NULL;
+    node->right = NULL;
+
+    return (node);
+}
+
+int main()
+{
+    struct node *root = newNode(1);
+
+    root->left = newNode(2);
+     root->right = newNode(3);
+
+     root->left->left = newNode(4);
+
+    cout<<root->left->left->data<<endl;
+     return 0;
+}
